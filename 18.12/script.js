90dba3912b733c178119dac64171f64cfacb8ef6
Troe <20bekarys04@gmail.com>
2024-12-19 15:19:01 +0500

    18.12

--- 18.12/script.js
+++ 18.12/script.js
@@ -35,21 +35,121 @@
 
 
 
-const cardsContainer = document.getElementById('cardsContainer');
+// const cardsContainer = document.getElementById('cardsContainer');
 
-const data = ["apple", "banana", "orange", "kiwi"];
+// const data = ["apple", "banana", "orange", "kiwi"];
 
-data.forEach((item) => {
+// data.forEach((item) => {
+//     const card = document.createElement('div');
+//     const title = document.createElement('h3');
+
+//     card.style.border = '1px solid black';
+//     card.style.borderRadius = '12px';
+//     card.style.padding = '10px';
+//     card.style.width = "fit-content";
+
+//     card.appendChild(title);
+//     title.textContent = item;
+
+//     const deleteButton = document.createElement('button');
+//     deleteButton.textContent = "Delete";
+
+//     card.appendChild(deleteButton);
+
+//     deleteButton.addEventListener('click', () => {
+//         card.remove();
+//     })
+
+//     cardsContainer.appendChild(card);
+// });
+
+
+
+// const data = [
+//     {title: "Абай жолы - Мұқтар Әуезов",
+//         description:"Абайдың өмірі мен қазақ қоғамының тарихи шежіресі"},
+//     {title: "Қан мен тер  - Әбдіжәміл Нұрпейсов",
+//         description:"Қазақ халқының қиын кезеңдерінің эпикалық баян."},
+//     {title: "lorem ipsum dolor sit amet", 
+//         description:"lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus."},
+//     {title: "lorem ipsum dolor sit amet",
+//         description:"lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus."},
+// ];
+// data.forEach((item) => {
+   
+//     const card = document.createElement('div');
+//     const title = document.createElement('h3');
+//     const description = document.createElement('p');
+
+
+// card.appendChild(title);
+// title.textContent = item; 
+
+// card.appendChild(description);
+// description.textContent = item.description;
+
+
+//  const deleteButton = document.createElement('button');
+// deleteButton.textContent = "Delete";
+
+// cardsContainer.appendChild(deleteButton);
+
+// deleteButton.addEventListener('click', () => {
+//     data.remove();
+// });
+// cardsContainer.appendChild(deleteButton);
+
+// })
+
+
+
+const data = [
+    {title: "Ruslan",
+        description:"13/02/2004",
+
+    },
+    {title:"Azamat",
+        description:"01/07/2006"
+    },
+    {
+        title:"Raushan",
+        description:"15/08/2004"
+    },
+    {
+        title:"Saule",
+        description:"21/09/2013"
+    }
+]
+
+data.forEach((element) =>{
     const card = document.createElement('div');
     const title = document.createElement('h3');
+    const description = document.createElement('p');
 
+    card.style.width = "200px";
     card.style.border = '1px solid black';
-    card.style.borderRadius = '12px';
-    card.style.padding = '10px';
-    card.style.width = "fit-content";
+    card.style.borderRadius ="5xp"
+    card.style.padding = "10px"
 
     card.appendChild(title);
-    title.textContent = item;
+    title.textContent = element.title;
+
+    card.appendChild(description);
+    description.textContent = element.description;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
 
     cardsContainer.appendChild(card);
-})
+    
+    deleteButton.addEventListener('click', () => {
+        card.remove();
+    });
+    card.appendChild(deleteButton);
+
+    card.addEventListener('dblclick', () => {
+        const comment = document.createElement('h3');
+        comment.textContent = "Hello!";
+        card.appendChild(comment);
+    })
+});
